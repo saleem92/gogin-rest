6cb80bf9fbb4fb1cd87c5d2520a8534b420b2b9a
saleem <salimmalikn@gmail.com>
2022-05-10 19:18:33 +0200

    Connect to postgres DB

---
+++ src/models/album.go
@@ -0,0 +1,8 @@
+package models
+
+type Album struct {
+	ID     string  `json:"id" binding:"required"`
+	Title  string  `json:"title" binding:"required"`
+	Artist string  `json:"artist" binding:"required"`
+	Price  float64 `json:"price" binding:"required"`
+}
